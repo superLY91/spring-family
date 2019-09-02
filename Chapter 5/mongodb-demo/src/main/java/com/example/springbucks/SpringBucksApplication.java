9ea032afdca0c75c342c4f8eda220268d4a119e5
liyang42 <liyang42@baidu.com>
2019-09-02 15:41:01 +0800

    add Chapter 5/mongodb-demo

---
+++ Chapter 5/mongodb-demo/src/main/java/com/example/springbucks/SpringBucksApplication.java
@@ -0,0 +1,100 @@
+package com.example.springbucks;
+
+import com.example.springbucks.coverter.MoneyReadConverter;
+import com.example.springbucks.coverter.MoneyWriteConverter;
+import com.example.springbucks.model.Coffee;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
+@SpringBootApplication
+public class SpringBucksApplication implements ApplicationRunner {
+    @Autowired
+    private ReactiveMongoTemplate mongoTemplate;
+    private CountDownLatch cdl = new CountDownLatch(2);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBucksApplication.class, args);
+    }
+
+    @Bean
+    public MongoCustomConversions mongoCustomConversions() {
+        return new MongoCustomConversions(
+                Arrays.asList(new MoneyReadConverter(), new MoneyWriteConverter()));
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+//        startFromInsertion(() -> log.info("Runnable"));
+        startFromInsertion(() -> {
+            log.info("Runnable");
+            decreaseHighPrice();
+        });
+
+        log.info("after starting");
+
+//        decreaseHighPrice();
+
+        cdl.await();
+    }
+
+    private void startFromInsertion(Runnable runnable) {
+        mongoTemplate.insertAll(initCoffee())
+                .publishOn(Schedulers.elastic())
+                .doOnNext(c -> log.info("Next: {}", c))
+                .doOnComplete(runnable)
+                .doFinally(s -> {
+                    cdl.countDown();
+                    log.info("Finally 1, {}", s);
+                })
+                .count()
+                .subscribe(c -> log.info("Inset {} record", c));
+
+    }
+
+    private void decreaseHighPrice() {
+        mongoTemplate.updateMulti(Query.query(Criteria.where("price").gte(3000L)),
+                new Update().inc("price", -500L).currentDate("updateTime"), Coffee.class)
+                .doFinally(s -> {
+                    cdl.countDown();
+                    log.info("Finally 2, {}", s);
+                })
+                .subscribe(r -> log.info("Result is {}", r));
+    }
+
+    private List<Coffee> initCoffee() {
+        Coffee espresso = Coffee.builder()
+                .name("espresso")
+                .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
+
+        Coffee latte = Coffee.builder()
+                .name("latte")
+                .price(Money.of(CurrencyUnit.of("CNY"), 30.0))
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
+
+        return Arrays.asList(espresso, latte);
+    }
+}
