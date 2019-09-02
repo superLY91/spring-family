ffeafe9229a735a13dc9442a7490ffe01c1a80b3
liyang <superly91@gamil.com>
2019-09-03 00:48:46 +0800

    add  Chapter 6/simple-controller-demo

---
+++ Chapter 6/simple-controller-demo/src/main/java/com/example/springbucks/repository/CoffeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.springbucks.repository;
+
+import com.example.springbucks.model.Coffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
+    List<Coffee> findByNameInOrderById(List<String> list);
+}
---
+++ Chapter 6/simple-controller-demo/src/main/java/com/example/springbucks/service/CoffeeService.java
@@ -0,0 +1,44 @@
+package com.example.springbucks.service;
+
+import com.example.springbucks.model.Coffee;
+import com.example.springbucks.repository.CoffeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.exact;
+
+@Slf4j
+@Service
+@CacheConfig(cacheNames = "CoffeeCache")
+public class CoffeeService {
+
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+
+    public Optional<Coffee> findOneCoffee(String name) {
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withMatcher("name", exact().ignoreCase());
+        Optional<Coffee> coffee = coffeeRepository.findOne(
+                Example.of(Coffee.builder().name(name).build(), matcher));
+        log.info("Coffee Found: {}", coffee);
+        return coffee;
+    }
+
+    @Cacheable
+    public List<Coffee> getAllCoffee() {
+        return coffeeRepository.findAll(Sort.by("id"));
+    }
+
+    public List<Coffee> getCoffeeByName(List<String> names) {
+        return coffeeRepository.findByNameInOrderById(names);
+    }
+}
