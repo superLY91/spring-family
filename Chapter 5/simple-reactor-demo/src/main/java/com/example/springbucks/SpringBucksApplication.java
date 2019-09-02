1c2810618df1953db2a70293e734bf7ed05b7eb8
liyang42 <liyang42@baidu.com>
2019-09-02 11:25:06 +0800

    finish Chapter 5/simple-reactor-demo

--- Chapter 5/simple-reactor-demo/src/main/java/com/example/springbucks/SpringBucksApplication.java
+++ Chapter 5/simple-reactor-demo/src/main/java/com/example/springbucks/SpringBucksApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 @Slf4j
@@ -20,19 +21,25 @@ public class SpringBucksApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Flux.range(1, 6)
-                .doOnRequest(n -> log.info("Request {} number", n))
-                .doOnComplete(() -> log.info("Publisher COMPLETE 1"))
+                .doOnRequest(n -> log.info("Request {} number", n)) // 注意顺序造成的区别
                 .publishOn(Schedulers.elastic())
+                .doOnComplete(() -> log.info("Publisher COMPLETE 1"))
                 .map(i -> {
                     log.info("Publish {}, {}", Thread.currentThread(), i);
 //                    return 10 / (i - 3);
                     return i;
                 })
                 .doOnComplete(() -> log.info("Publisher COMPLETE 2"))
-//                .subscribeOn(Schedulers.single())
+                .subscribeOn(Schedulers.single())
+                .onErrorResume(e -> {
+                    log.error("Exception {}", e.toString());
+                    return Mono.just(-1);
+                })
+//                .onErrorReturn(-1)
                 .subscribe(i -> log.info("Subscribe {}: {}", Thread.currentThread(), i),
                         e -> log.error("error {}", e.toString()),
-                        () -> log.info("Subscriber COMPLETE"));
-
+                        () -> log.info("Subscriber COMPLETE"),
+                        s -> s.request(4));
+        Thread.sleep(2000);
     }
 }
