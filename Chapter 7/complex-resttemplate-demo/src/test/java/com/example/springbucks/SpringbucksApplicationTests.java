ef275d478b87833290a8769ce386aba2f4e02c60
liyang42 <liyang42@baidu.com>
2019-09-04 14:31:19 +0800

    add  Chapter 7/complex-resttemplate-demo

---
+++ Chapter 7/complex-resttemplate-demo/src/test/java/com/example/springbucks/SpringbucksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbucks;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
